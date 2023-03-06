49611e3aa99c9bd86f51e9f00d5ac9678a2ad866
Eugene Tarasenko <brumby0890@gmail.com>
2023-03-06 23:23:19 +0200

    updated

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ let userLastName = prompt('Enter your surname:').trim();
 userLastName = userLastName[0].toUpperCase() + userLastName.slice(1).toLowerCase();
 let userEmail = prompt('Enter your email:').replaceAll(/\s+/g, '').toLowerCase();
 
-if (!userEmail.includes('@')) userEmail = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
-else if (userEmail.indexOf('@') === 0) userEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
-else if (userEmail.indexOf('@') === userEmail.length - 1) userEmail = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
-else userEmail = `<b>${userEmail}</b>`;
+let emailEntry;
+if (!userEmail.includes('@')) {
+  emailEntry = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
+} else if (userEmail.startsWith('@')) {
+  emailEntry = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
+} else if (userEmail.endsWith('@')) {
+  emailEntry = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
+} else { emailEntry = `<b>${userEmail}</b>`; }
 
 const userBirthYear = +prompt('Enter your year of birth:').replaceAll(/\s+/g, '');
 const userAge = new Date().getFullYear() - userBirthYear;
@@ -18,7 +22,7 @@ const listItem1 = document.createElement('li');
 const listItem2 = document.createElement('li');
 const listItem3 = document.createElement('li');
 listItem1.innerHTML = `Full name: <b>${userFirstName} ${userLastName}</b>`;
-listItem2.innerHTML = `Email: ${userEmail}`;
+listItem2.innerHTML = `Email: ${emailEntry}`;
 listItem3.innerHTML = `Age: <b>${userAge}</b>`;
 list.appendChild(listItem1);
 list.appendChild(listItem2);
