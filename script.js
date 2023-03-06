627f6b3836757b4657775cc38ca0e40ebf12b2ad
Eugene Tarasenko <brumby0890@gmail.com>
2023-03-06 21:37:41 +0200

    updated

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let name = prompt('Enter your name:').trim();
-name = name[0].toUpperCase() + name.slice(1).toLowerCase();
-let surname = prompt('Enter your surname:').trim();
-surname = surname[0].toUpperCase() + surname.slice(1).toLowerCase();
-let email = prompt('Enter your email:').replaceAll(/\s+/g, '').toLowerCase();
+let userFirstName = prompt('Enter your name:').trim();
+userFirstName = userFirstName[0].toUpperCase() + userFirstName.slice(1).toLowerCase();
+let userLastName = prompt('Enter your surname:').trim();
+userLastName = userLastName[0].toUpperCase() + userLastName.slice(1).toLowerCase();
+let userEmail = prompt('Enter your email:').replaceAll(/\s+/g, '').toLowerCase();
 
-if (!email.includes('@')) email = `not valid email <b>${email}</b> (symbol @ not exist)`;
-else if (email.indexOf('@') === 0) email = `not valid email <b>${email}</b> (symbol @ find in first place)`;
-else if (email.indexOf('@') === email.length - 1) email = `not valid email <b>${email}</b> (symbol @ find in last place)`;
-else email = `<b>${email}</b>`;
+if (!userEmail.includes('@')) userEmail = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
+else if (userEmail.indexOf('@') === 0) userEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
+else if (userEmail.indexOf('@') === userEmail.length - 1) userEmail = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
+else userEmail = `<b>${userEmail}</b>`;
 
-const birthYear = +prompt('Enter your year of birth:').replaceAll(/\s+/g, '');
-const age = new Date().getFullYear() - birthYear;
+const userBirthYear = +prompt('Enter your year of birth:').replaceAll(/\s+/g, '');
+const userAge = new Date().getFullYear() - userBirthYear;
 
 const list = document.createElement('ul')
 document.querySelector('body').appendChild(list);
 const listItem1 = document.createElement('li');
 const listItem2 = document.createElement('li');
 const listItem3 = document.createElement('li');
-listItem1.innerHTML = `Full name: <b>${name} ${surname}</b>`;
-listItem2.innerHTML = `Email: ${email}`;
-listItem3.innerHTML = `Age: <b>${age}</b>`;
+listItem1.innerHTML = `Full name: <b>${userFirstName} ${userLastName}</b>`;
+listItem2.innerHTML = `Email: ${userEmail}`;
+listItem3.innerHTML = `Age: <b>${userAge}</b>`;
 list.appendChild(listItem1);
 list.appendChild(listItem2);
 list.appendChild(listItem3);
