5d6b638a54a8010dd9c41ce16544daa3537ae5a5
Eugene Tarasenko <87163666+EugeneTarasenko@users.noreply.github.com>
2023-06-02 05:03:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getRandomColor() {
     const color1 = Math.floor(Math.random() * 256);
     const color2 = Math.floor(Math.random() * 256);
     const color3 = Math.floor(Math.random() * 256);
-    return "rgb(" + color1 + ", " + color2 + ", " + color3 + ")";
+    return `rgb(${color1},${color2},${color3})`;
 }
 
 function changeColor() {
@@ -18,8 +18,8 @@ function moveSquare() {
     const randomLeft = Math.floor(Math.random() * (bodyWidth - 100));
     const randomTop = Math.floor(Math.random() * (bodyHeight - 100));
 
-    block.style.left = randomLeft + 'px';
-    block.style.top = randomTop + 'px';
+    block.style.left = `${randomLeft}px`;
+    block.style.top = `${randomTop}px`;
 }
 
 setInterval(changeColor, 500);
